b0dd006a94e961d720794be7fe5c6a92c9ac5c45
Aaron Schachter <aaronschachter@gmail.com>
2016-07-19 18:03:18 -0700

    Initial Commit

---
+++ teamio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  teamio
+//
+//  Created by Aaron Schachter on 7/19/16.
+//  Copyright © 2016 New School Old School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ teamio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  teamio
+//
+//  Created by Aaron Schachter on 7/19/16.
+//  Copyright © 2016 New School Old School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
